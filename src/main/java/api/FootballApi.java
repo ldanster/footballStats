26101bf8c9fd237b607abbf9b9041febf606fe44
Lebohang Danster <frdmforce@hotmail.com>
2016-10-29 00:21:47 +0200

    Changes aren't really that special

--- src/main/java/api/FootballApi.java
+++ src/main/java/api/FootballApi.java
@@ -5,6 +5,7 @@ import com.google.gson.reflect.TypeToken;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Response;
 
+import java.io.IOException;
 import java.util.List;
 
 public class FootballApi {
@@ -12,18 +13,21 @@ public class FootballApi {
     OkHttpClient client = new OkHttpClient();
 
     public <T> T getResponse (FootballRequestBuilder builder) throws Exception {
-        Response response = client.newCall(builder.buildRequest()).execute();
+        Response response = buildResponse(builder);
 
         Gson gson = new Gson();
-
         return gson.fromJson(response.body().string().toString(), new TypeToken<List<T>>(){}.getType());
     }
 
     public <T> T getResponse(FootballRequestBuilder builder, Class<T> genericClass) throws Exception {
-        Response response = client.newCall(builder.buildRequest()).execute();
+        Response response = buildResponse(builder);
 
         Gson gson = new Gson();
         return gson.fromJson(response.body().string().toString(), genericClass);
+    }
+
+    private Response buildResponse(FootballRequestBuilder builder) throws IOException {
+        return client.newCall(builder.buildRequest()).execute();
     }
 
 }
